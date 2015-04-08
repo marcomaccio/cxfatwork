c8411502d7e68f2e1ea402681088ce65dac8ceb0
Marco Maccio <marco.maccio@marmac.name>
2015-04-08 21:23:26 +0200

    Updated the mysql DDL
    Updated the MySQL DDL script to create the table used to persist the
    Persistence Object

--- cxfatwork-rs-j2ee/cxfatwork-rs-j2ee-dal-impl-jpa/src/integration-test/data/schema-mysql.sql
+++ cxfatwork-rs-j2ee/cxfatwork-rs-j2ee-dal-impl-jpa/src/integration-test/data/schema-mysql.sql
@@ -1,11 +1,23 @@
-CREATE TABLE Customers (
-  pkid            int(10) NOT NULL AUTO_INCREMENT,
-  firstname     varchar(255) NOT NULL,
-  lastname      varchar(255) NOT NULL,
-  customerId    int(10) NOT NULL,
-  createDate    datetime DEFAULT NULL,
-  lastUpdate    datetime DEFAULT NULL,
-  PRIMARY KEY (id),
+DROP TABLE IF EXISTS gestimmo.CUSTOMERS;
+
+DROP TABLE IF EXISTS gestimmo.SEQUENCE;
+
+CREATE TABLE gestimmo.SEQUENCE (
+  SEQ_NAME varchar(50) NOT NULL,
+  SEQ_COUNT decimal(38,0) DEFAULT NULL,
+  PRIMARY KEY (SEQ_NAME)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE gestimmo.CUSTOMERS (
+  pkId          bigint(20)    NOT NULL,
+  customerId    varchar(255)  NOT NULL,
+  firstname     varchar(255)  NOT NULL,
+  lastname      varchar(255)  NOT NULL,
+  createDate    datetime      DEFAULT NULL,
+  lastUpdate    datetime      DEFAULT NULL,
+  appUser       varchar(255)  DEFAULT NULL,
+  version       bigint(20)    DEFAULT NULL,
+  PRIMARY KEY (pkId),
   UNIQUE KEY customerId (customerId)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
